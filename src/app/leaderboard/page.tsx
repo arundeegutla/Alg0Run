64961a10b6581c91c403cb90eb29d0aa5686cc8d
Arun Deegutla <arundeegutla2004@gmail.com>
2023-10-07 06:49:20 -0400

    user-auth-and-skeleton

---
+++ src/app/leaderboard/page.tsx
@@ -0,0 +1,15 @@
+import ListGroup from '@/components/ListGroup';
+import NavBar from '@/components/NavBar';
+import Image from 'next/image';
+
+export default function Home() {
+    return (
+        <main className="default items-center justify-center flex-col px-[20%]">
+            <NavBar current='Leaderboard'></NavBar>
+            <h1 className='text-4xl'>Leaderboard</h1>
+            <div className="flex flex-col w-[50%]">
+                <ListGroup />
+            </div>
+        </main>
+    );
+}
