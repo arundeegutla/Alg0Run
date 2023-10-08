867b787b778194e833b1640028176d72c9a0c122
Rahul Mohan <45293734+rahulmohan126@users.noreply.github.com>
2023-10-07 23:14:46 -0400

    Switched to POST

--- back-end/src/routes/leaderboard.ts
+++ back-end/src/routes/leaderboard.ts
@@ -3,11 +3,11 @@ import { Request, Response } from "express"
 import * as controller from "../controllers/leaderboard"
 const router = require('express').Router()
 
-router.get('/algo', async (req: Request, res: Response) => {
+router.post('/algo', async (req: Request, res: Response) => {
   res.json(await controller.getAlgoLeaderboard(req.body.algoId as string));
 });
 
-router.get('/users', async (req: Request, res: Response) => {
+router.post('/users', async (req: Request, res: Response) => {
   res.json(await controller.getUserLeaderboard());
 });
 
