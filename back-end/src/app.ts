bb4e744ac14faa71393855738393994ae0f1ea10
Rahul Mohan <45293734+rahulmohan126@users.noreply.github.com>
2023-10-08 02:57:24 -0400

    Final backend commit

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,21 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-import { getProfile, getProfileByToken } from "./controllers/profile";
-import { createProfile } from "./services/user";
 const app = express();
-const router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Define your API routes here
-
-app.post("/", async (req, res) => {
-  // const r = await getProfile("skf5ViXIrFge6X13T5s2XB6JWEJ3");
-  const r = await createProfile("id123", "bigman1")
-  res.send(r);
-});
 
 app.use('/api/algo', require('./routes/algo'))
 app.use('/api/leaderboard', require('./routes/leaderboard'))
