5dc8bad997a97a78efa725a39f8d420f40420d09
Arun Deegutla <arundeegutla2004@gmail.com>
2024-11-28 20:08:42 -0500

    testing apis

--- pages/api/algo/[slug].ts
+++ pages/api/algo/[slug].ts
@@ -53,7 +53,7 @@ export default async function handler(
           return res.status(404).json({ error: 'Endpoint not found ' + slug });
       }
     } else {
-      return res.status(405).json({ error: 'Method not allowed' });
+      return res.status(405).json({ error: 'Method not allowed' + method });
     }
   } catch (error) {
     console.error(`Error in ${slug}:`, error);
